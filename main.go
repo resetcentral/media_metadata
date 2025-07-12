7c103a65693963bd905acf63ba3108cb74bc5ad4
Nic Mews-Schmuck <resetcentral@proton.me>
2025-07-12 14:11:26 -0700

    remove test data from main

--- main.go
+++ main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"log"
-	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/resetcentral/media_library/handlers"
-	"github.com/resetcentral/media_library/models"
 	"github.com/resetcentral/media_library/storage"
 )
 
@@ -16,28 +14,8 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	testAudio := models.AudioMedia{
-		Title:         "Test Audio 2",
-		Description:   "This is a test audio",
-		FilePath:      "/path/to/file2.mp3",
-		Studio:        models.Studio{Name: "Dummy Studio"},
-		Artists:       []models.Artist{{Name: "Artist A"}, {Name: "Artist B"}},
-		ReleaseDate:   time.Now(),
-		Tags:          []models.Tag{{Value: "Cool"}, {Value: "Fun"}},
-		Length:        time.Duration(5 * 1e9),
-		Container:     "mp3",
-		AudioCodec:    "aac",
-		AudioChannels: 1,
-	}
-	err = storage.DB.CreateMedia(&testAudio)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	log.Printf("%#v\n", testAudio)
-
 	router := gin.Default()
-	router.GET("/artist", handlers.GetAllArtists)
+	router.GET("/artist", handlers.GetArtists)
 	router.GET("/artist/:id", handlers.GetArtistByID)
 	router.POST("/artist", handlers.PostArtist)
 	router.DELETE("/artist/:id", handlers.DeleteArtist)
